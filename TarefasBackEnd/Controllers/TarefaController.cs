b19a04d9e7b319f7c04aac49ef8ec7b3cdf6aa75
Daniel Fernandes <danielfernandesp1@gmail.com>
2023-07-28 07:36:15 -0300

    Created API Controller Class

---
+++ TarefasBackEnd/Controllers/TarefaController.cs
@@ -0,0 +1,14 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace TarefasBackEnd.Controllers
+{
+    [ApiController]
+    [Route("tarefa")]
+
+    public class TarefaController : ControllerBase
+    {
+        public IActionResult Get(){
+            return Ok();
+        }
+    }
+}
